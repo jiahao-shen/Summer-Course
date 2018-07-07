10b9a1794811eecc4a05a0bdcb3acdca3e4d2291
shenjiahao <1843781563@qq.com>
2018-07-08 02:39:01 +0800

    Add pi_mpi_openmp.c

--- Week1/Day5/pi_mpi_openmp.c
+++ Week1/Day5/pi_mpi_openmp.c
@@ -1,5 +1,7 @@
-# include <stdio.h>
-# include "mpi.h"
+#include <stdio.h>
+#include <omp.h>
+#include "mpi.h"
+#define NUM_THREADS 4
 double f(double x) {
     return 4 / (1 + x * x);
 }
@@ -8,15 +10,18 @@ int main(int argc, char** argv) {
     int num_process, process_id;
     long long n = 100000000;
     double start_time, end_time;
+    double dx = 1.0 / n;
+
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
     MPI_Comm_size(MPI_COMM_WORLD, &num_process);
+    omp_set_num_threads(NUM_THREADS);
 
     start_time = MPI_Wtime();
 
-    double dx = 1.0 / n;
     double sum = 0.0;
+    #pragma omp parallel for reduction(+: sum)
     for (int i = process_id + 1; i <= n; i += num_process) {
         sum += f(dx * (i - 0.5));
     }
@@ -25,8 +30,8 @@ int main(int argc, char** argv) {
 
     if (process_id == 0) {
         end_time = MPI_Wtime();
-        printf("Time:%fs\n", end_time - start_time);
+		printf("Time:%fs\n", end_time - start_time);
         printf("%0.8f\n", pi);
     }
-    MPI_Finalize();
-}
+    MPI_Finalize();   
+}   
